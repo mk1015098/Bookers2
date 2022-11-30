eda0e411c14fd14220d8c734de3ef0485bcf9484
MoekaKakimoto <k.moeka0984@gmail.com>
2022-11-30 13:00:17 +0000

    [Update]bookers-debug2

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+  has_many :books, dependent: :destroy
+
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+
+
+end
