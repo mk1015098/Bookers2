c8b50c30212903d07789575ad2dcba5bae84953e
MoekaKakimoto <k.moeka0984@gmail.com>
2022-12-02 08:37:09 +0000

    [Finish]bookers3

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   end
 
   has_many :books, dependent: :destroy
-
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-
 
 end
