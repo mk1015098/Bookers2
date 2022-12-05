6f75d7b72f26c5acd1eb9c3293ee555116df2e8b
MoekaKakimoto <k.moeka0984@gmail.com>
2022-12-05 12:03:57 +0000

    [Finish]bookers5

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
-  
+  get "search" => "searches#search"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create,:destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
-  
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
